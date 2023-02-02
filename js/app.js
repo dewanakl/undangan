189400cbc0fdb096fb63ccb503e70207c8541245
dewanakl <dewanakretarta29@gmail.com>
2023-02-02 23:59:51 +0700

    feat: add feature ucapan

--- js/app.js
+++ js/app.js
@@ -41,26 +41,25 @@ const play = (btn) => {
 
 const renderCard = (data) => {
     const DIV = document.createElement('div');
-    DIV.classList.add('mb-4');
+    //DIV.classList.add('mb-3');
+    DIV.setAttribute('data-aos', 'fade-up')
     DIV.innerHTML = `
-    <div class="card-body shadow p-3 rounded-3">
-        <div class="d-flex justify-content-between align-items-center">
-            <h6 class="text-truncate m-0 p-0" style="max-width: 40%;">
-                <strong>${data.nama}</strong>
-            </h6>
-            <small class="text-light m-0 p-0">
-                ${data.created_at}
-            </small>
-        </div>
-        <hr class="mt-1 mb-2">
-        <small class="text-start mt-2 mb-1 mx-0 p-0">${data.komentar}</small>
+    <div class="card-body bg-light shadow p-3 rounded-3 mb-3">
+        <h6 class="text-dark m-0 p-0">
+            <strong>${data.nama}</strong>
+        </h6>
+        <small class="text-dark m-0 p-0">
+            ${data.created_at}
+        </small>
+        <hr class="text-dark mt-1 mb-2">
+        <small class="text-dark mt-2 mb-1 mx-0 p-0">${data.komentar}</small>
     </div>`;
     return DIV;
 };
 
 const ucapan = async () => {
     const UCAPAN = document.getElementById('daftarucapan');
-    UCAPAN.innerHTML = `<span class="spinner-border spinner-border-sm me-1"></span>Loading...`;
+    UCAPAN.innerHTML = `<div class="text-center"><span class="spinner-border spinner-border-sm me-1"></span>Loading...</div>`;
 
     await fetch('https://undangan-api-gules.vercel.app/api/comment')
         .then((res) => res.json())
@@ -93,6 +92,56 @@ const login = async () => {
             token = res.data.token;
         })
         .catch((err) => alert(err));
+};
+
+const kirim = async () => {
+    let nama = document.getElementById('formnama').value;
+    let komentar = document.getElementById('formpesan').value;
+
+    if (nama.length == 0) {
+        alert('nama tidak boleh kosong');
+        return;
+    }
+
+    if (komentar.length == 0) {
+        alert('pesan tidak boleh kosong');
+        return;
+    }
+
+    document.getElementById('kirim').disabled = true;
+    document.getElementById('kirim').innerHTML = `<span class="spinner-border spinner-border-sm me-1"></span>Loading...`;
+
+    const REQ = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        },
+        body: JSON.stringify({
+            nama: nama,
+            hadir: false,
+            komentar: komentar
+        })
+    };
+
+    await fetch('https://undangan-api-gules.vercel.app/api/comment', REQ)
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.code == 201) {
+                ucapan();
+            }
+
+            if (res.error) {
+                alert(Object.values(res.error)[0]);
+            }
+        })
+        .catch((err) => alert(err));
+
+    document.getElementById('formnama').value = null;
+    document.getElementById('formpesan').value = null;
+    document.getElementById('kirim').disabled = false;
+    document.getElementById('kirim').innerHTML = `Kirim<i class="fa-solid fa-paper-plane ms-1"></i>`;
 };
 
 document.addEventListener('DOMContentLoaded', () => {
